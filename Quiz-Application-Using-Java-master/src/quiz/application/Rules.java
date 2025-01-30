735f85568140cb9631b28bbc289e68706ccb8e03
rohittarke10 <145198820+rohittarke10@users.noreply.github.com>
2025-01-30 23:14:22 +0530

    Add files via upload

---
+++ Quiz-Application-Using-Java-master/src/quiz/application/Rules.java
@@ -0,0 +1,74 @@
+package quiz.application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Rules extends JFrame implements ActionListener{
+
+    String name;
+    JButton start, back;
+    
+    Rules(String name) {
+        this.name = name;
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("Welcome " + name + " to Simple Minds");
+        heading.setBounds(50, 20, 700, 30);
+        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 28));
+        heading.setForeground(new Color(30, 144, 254));
+        add(heading);
+        
+        JLabel rules = new JLabel();
+        rules.setBounds(20, 90, 700, 350);
+        rules.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        rules.setText(
+            "<html>"+ 
+                "1. Read each question carefully before selecting an answer." + "<br><br>" +
+    "2. Each question carries equal marks unless specified otherwise." + "<br><br>" +
+    "3. There is no negative marking (unless mentioned)." + "<br><br>" +
+    "4. Select only one option unless instructed otherwise." + "<br><br>" +
+    "5. Do not refresh or navigate away from the test page." + "<br><br>" +
+    "6. Once you submit your answer, you cannot change it." + "<br><br>" +
+    "7. Ensure a stable internet connection to avoid test disruptions." + "<br><br>" +
+    "8. Any form of cheating or plagiarism will lead to disqualification." + "<br><br>" +
+    "9. The timer will continue running even if you log out." + "<br><br>" +
+    "10. Submit the test before the deadline to avoid automatic submission." + "<br><br>" +
+            "<html>"
+        );
+        add(rules);
+        
+        back = new JButton("Back");
+        back.setBounds(250, 500, 100, 30);
+        back.setBackground(new Color(30, 144, 254));
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        add(back);
+        
+        start = new JButton("Start");
+        start.setBounds(400, 500, 100, 30);
+        start.setBackground(new Color(30, 144, 254));
+        start.setForeground(Color.WHITE);
+        start.addActionListener(this);
+        add(start);
+        
+        setSize(800, 650);
+        setLocation(350, 100);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == start) {
+            setVisible(false);
+            new Quiz(name);
+        } else {
+            setVisible(false);
+            new Login();
+        }
+    }
+    
+    public static void main(String[] args) {
+        new Rules("User");
+    }
+}
